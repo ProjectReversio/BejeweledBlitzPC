b7d9a9f6c50b1c1f4b5c76204b75c7788fd183c7
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2021-06-17 22:08:29 -0500

    Implemented SexyApp::Init

--- src/BejBlitz/SexyApp.cpp
+++ src/BejBlitz/SexyApp.cpp
@@ -1,3 +1,6 @@
+#include <SexyAppFramework/SEHCatcher.h>
+#include <SexyAppFramework/Debug.h>
+#include <ImageLib/ImageLib.h>
 #include "SexyApp.h"
 
 namespace Sexy
@@ -43,7 +46,27 @@ namespace Sexy
 
     void SexyApp::Init()
     {
-        // TODO: Implement Init
+        Sexy::SEHCatcher::mCrashMessage = SexyStringToWString(GetString("UNEXPECTED_ERROR",
+                  L"An unexpected error has occured!  Pressing 'Send Report' will send us helpful debugging information that may help us"
+                  " resolve this issue in the future.\r\n"
+                  "\r\n"
+                  "You can also contact us directly at feedback@popcap.com."));
+
+        Sexy::SEHCatcher::mSubmitMessage = SexyStringToWString(GetString("PLEASE_HELP",
+                   L"Please help us out by providing as much information as you can about this crash. Is this the first time it happened?"
+                   " Have you used other PopCap Deluxe games successfully before? Have you upgraded your drivers or any software recent"
+                   "ly that may be interfering with this program?"));
+
+        Sexy::SEHCatcher::mSubmitErrorMessage = SexyStringToWString(GetString("FAILED_CONNECT_POPCAP",
+                  L"Failed to connect to PopCap servers.  Please check your Internet connection.\n"
+                  "If you are on a dial-up connection, you may have to manually connect to your ISP."));
+
+        Sexy::SEHCatcher::mSubmitHost = "www.popcap.com";
+
+        OutputDebug(_S("Product: %s\r\n"), StringToSexyString(mProdName).c_str());
+        OutputDebug(_S("BuildDate: %s\r\n"), StringToSexyString(mBuildDate).c_str());
+
+        ImageLib::SetJ2KCodecKey("LU5OkT7!L06fiD9nMbCE9ZoJuXxgx4zcgVDd2!a1tb2uHfhRrLG5wnNySQswXqAt2h");
 
         Parent::Init();
         ++mTimesExecuted;
